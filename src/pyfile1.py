4c6c5d29678cf71663c2caa08fc5019a62d2f436
Nadav Har Tzvi <nadavha@apache.org>
2019-03-06 08:56:47 +0200

    use the sdk!

--- src/pyfile1.py
+++ src/pyfile1.py
@@ -1,7 +1,10 @@
+from amaterasu.pyspark.runtime import ama_context
 import os
+from pyspark import Row
+
 data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 print(os.listdir(os.getcwd()))
-rdd = sc.parallelize(data)
+rdd = ama_context.sc.parallelize(data)
 numDS = rdd.map(Row).toDF()
 
 def g(x):
