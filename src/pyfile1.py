6791528b933c46800382a46d19f7309c8f6ee3e8
Nadav Har Tzvi <nadavha@apache.org>
2019-03-16 12:15:02 +0200

    using new builder

--- src/pyfile1.py
+++ src/pyfile1.py
@@ -1,6 +1,8 @@
-from amaterasu.pyspark.runtime import ama_context
+from amaterasu.pyspark.runtime import AmaContext
 import os
 from pyspark import Row
+
+ama_context: AmaContext = AmaContext.builder().build()
 
 data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 print(os.listdir(os.getcwd()))
