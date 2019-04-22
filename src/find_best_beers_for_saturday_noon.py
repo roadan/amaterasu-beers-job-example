6f8fba45921370b8363a52a10712ad0ed0547173
Yaniv Rodenski <roadan@gmail.com>
2019-04-22 13:31:26 +1000

    Update find_best_beers_for_saturday_noon.py

--- src/find_best_beers_for_saturday_noon.py
+++ src/find_best_beers_for_saturday_noon.py
@@ -3,7 +3,7 @@ import requests
 
 ama_context = AmaContext.builder().build()
 brewery_db_conf = ama_context.dataset_manager.get_dataset_configuration("brewerydb-styles")
-r = requests.get(brewery_db_conf['uri'], params={'key': ama_context.env['configurations']['breweryDBApiKey']})
+r = requests.get(brewery_db_conf['uri'], params={'key': ama_context.env['configuration']['breweryDBApiKey']})
 styles_df = ama_context.spark.createDataFrame(r.json()['data']).select('id', 'shortName')
 beers_df = ama_context.get_dataset("random-beers").select('abv', 'styleId', 'name')
 joined_df = beers_df.join(styles_df, on=beers_df.styleId == styles_df.id).drop('id', 'styleId')
